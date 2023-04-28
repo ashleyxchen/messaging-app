2bb68531f282ef8cf36a2bd2a5ceef795a9bbf90
Ashley Chen <86508619+ashleyxchen@users.noreply.github.com>
2023-04-28 16:34:34 -0400

    add components

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,30 +1,3 @@
-// import React, { useContext } from 'react'
-// import { AuthContext } from '../context/AuthContext'
-// import { ChatContext } from '../context/ChatContext'
-
-// const Message = () => {
-//   const {currentUser} = useContext(AuthContext)
-//   const {data} = useContext(ChatContext)
-  
-//   return (
-//     <div className='message owner'>
-//       <div className='messageInfo'>
-//       <img
-//           src=""
-//           alt=""
-//         /> 
-//         <span>Just now</span>
-//       </div>
-      
-//       <div className="messageContent">
-//         <p>hello</p>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Message
-
 import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
