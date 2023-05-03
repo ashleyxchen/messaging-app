669657530f19dd556c888660941b15780caa791f
Ashley Chen <86508619+ashleyxchen@users.noreply.github.com>
2023-05-03 17:11:40 -0400

    style

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
+// import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
     <ChatContextProvider>
       <React.StrictMode>
-        <App />
+        {/* <ChakraProvider> */}
+          <App />
+        {/* </ChakraProvider> */}
       </React.StrictMode>
     </ChatContextProvider>
   </AuthContextProvider>
